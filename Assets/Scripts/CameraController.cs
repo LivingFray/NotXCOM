2e107e4ae2a85914125d6a9990d1a42537f25449
Michael Maskell <michael.maskell@live.co.uk>
2018-07-28 17:06:09 +0100

    Added camera panning

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float edgeSize = 0.05f;
+
+    public float cameraSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float normX = Input.mousePosition.x / Screen.width;
+        float normY = Input.mousePosition.y / Screen.height;
+
+        if (normX < edgeSize) {
+            transform.localPosition -= transform.right * Time.deltaTime * cameraSpeed;
+        } else if (normX > 1.0 - edgeSize) {
+            transform.localPosition += transform.right * Time.deltaTime * cameraSpeed;
+        }
+
+        if (normY < edgeSize) {
+            transform.localPosition -= Vector3.Cross(transform.right, new Vector3(0, 1, 0)) * Time.deltaTime * cameraSpeed;
+        } else if (normY > 1.0 - edgeSize) {
+            transform.localPosition += Vector3.Cross(transform.right, new Vector3(0, 1, 0)) * Time.deltaTime * cameraSpeed;
+        }
+    }
+}
