6813e5343f6d59869877fc061c9b4901b90c21b1
Michael Maskell <michael.maskell@live.co.uk>
2018-07-30 01:50:57 +0100

    Added hit chances

--- Assets/Scripts/EntityController.cs
+++ Assets/Scripts/EntityController.cs
@@ -17,6 +17,20 @@ public class EntityController : MonoBehaviour {
 
     public Vector3Int GridPos { get; private set; }
 
+    public int health = 5;
+
+    //Other stats like aim, defence, etc here
+
+    //TODO: Refactor attacks to be separate class
+    //TODO: Make damage dealt a stat of weapon
+    public int minDamage = 3;
+    public int maxDamage = 5;
+
+    public float aim = 0.6f;
+
+    public float distHitBase = 0.4f;
+    public float distHitFall = 0.045f;
+
     public void SetPosition(Vector3Int position) {
         GridPos = position;
         transform.position = position;
@@ -60,12 +74,48 @@ public class EntityController : MonoBehaviour {
         byte cover;
         bool los = controller.HasLineOfSightDDA(GridPos, enemy.GridPos, out cover);
         if (los) {
-            Debug.Log("Took shot at enemy " + cover);
+            float hitChance = GetHitChance(enemy, cover);
+            if(Random.Range(0.0f, 1.0f) < hitChance) {
+                Debug.Log("Hit target (" + hitChance * 100.0f + "%)");
+                int damage = Random.Range(minDamage, maxDamage + 1);
+                enemy.Damage(damage);
+            } else {
+                Debug.Log("Missed target (" + hitChance * 100.0f + "%)");
+            }
             //Pretty fire animations and such
         } else {
             Debug.Log("Shot blocked");
         }
         actions--;
+    }
+
+    public float GetHitChance(EntityController enemy, byte coverType) {
+        //Debug.Log("Aim: " + aim);
+        //TODO: Make calculation more flexible / setting based
+        //Calculate hit chance:
+        //TODO: Aiming angles: better angle = better chance
+        float distanceModifier = distHitBase - distHitFall * (GridPos - enemy.GridPos).magnitude;
+        //Debug.Log("Distance: " + distanceModifier);
+        float coverModifier;
+        if(coverType == (byte)CoverType.FULL) {
+            coverModifier = -0.4f;
+        } else if (coverType == (byte)CoverType.HALF) {
+            coverModifier = -0.2f;
+        } else {
+            coverModifier = 0.0f;
+        }
+        //Debug.Log("Cover: " + coverModifier);
+        return aim + distanceModifier + coverModifier;
+    }
+
+    public void Damage(int damage) {
+        health -= damage;
+        if(health <= 0) {
+            health = 0;
+            //Do death
+            //Doesn't remove from list yet
+            enabled = false;
+        }
     }
 
     private void Awake() {
