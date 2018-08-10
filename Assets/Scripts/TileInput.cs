8268f5655935b23e7686bbef373b9413d825b120
Michael Maskell <michael.maskell@live.co.uk>
2018-08-10 23:58:51 +0100

    Changed autogenerator to only create tiles at edge of bounding boxes

--- Assets/Scripts/TileInput.cs
+++ Assets/Scripts/TileInput.cs
@@ -21,17 +21,23 @@ public class TileInput : MonoBehaviour {
     }
 
     private void OnValidate() {
+        UpdateFaces();
+    }
+
+    public void UpdateFaces() {
         transform.Find("NegX").gameObject.SetActive(negativeX);
         transform.Find("PosX").gameObject.SetActive(positiveX);
         transform.Find("NegY").gameObject.SetActive(negativeY);
         transform.Find("PosY").gameObject.SetActive(positiveY);
         transform.Find("NegZ").gameObject.SetActive(negativeZ);
         transform.Find("PosZ").gameObject.SetActive(positiveZ);
+        //Handle floor hitbox
+        GetComponent<BoxCollider>().enabled = negativeY;
     }
 
     [MenuItem("NotXCOM/Autogenerate Tiles", true)]
     static bool CreateTilesValidate() {
-        if(!Selection.activeTransform || GameObject.Find("Board") == null) {
+        if (!Selection.activeTransform || GameObject.Find("Board") == null) {
             return false;
         }
         return true;
@@ -42,24 +48,24 @@ public class TileInput : MonoBehaviour {
         foreach (GameObject selected in Selection.gameObjects) {
             Transform transform = selected.transform;
             BoxCollider boxCollider = selected.GetComponent<BoxCollider>();
-            if(!boxCollider) {
+            if (!boxCollider) {
                 continue;
             }
             //Get edges of bounding box
             Vector3 minPos = transform.position + boxCollider.center - Vector3.Scale(transform.localScale, boxCollider.size) / 2.0f;
             Vector3 maxPos = transform.position + boxCollider.center + Vector3.Scale(transform.localScale, boxCollider.size) / 2.0f;
             //Move in from edges to centre
-            minPos += new Vector3(0.5f, 0.5f, 0.5f);
-            maxPos -= new Vector3(0.5f, 0.5f, 0.5f);
+            minPos += new Vector3(0.5f, 0.0f, 0.5f);
+            maxPos -= new Vector3(0.5f, 1.0f, 0.5f);
             Vector3Int minIntPos = Vector3Int.RoundToInt(minPos);
             Vector3Int maxIntPos = Vector3Int.RoundToInt(maxPos);
-            //For now just place edges
             GameObject holder = new GameObject(transform.name);
             holder.transform.parent = board;
+            //For each tile inside bounding box, attempt to place a tile
             for (int x = minIntPos.x; x <= maxIntPos.x; x++) {
                 for (int y = minIntPos.y; y <= maxIntPos.y; y++) {
                     for (int z = minIntPos.z; z <= maxIntPos.z; z++) {
-                        PlaceTile(holder.transform, new Vector3Int(x, y, z));
+                        PlaceTile(holder.transform, new Vector3Int(x, y, z), minIntPos, maxIntPos);
                     }
                 }
             }
@@ -67,8 +73,27 @@ public class TileInput : MonoBehaviour {
         }
     }
 
-    static void PlaceTile(Transform parent, Vector3Int pos) {
+    static void PlaceTile(Transform parent, Vector3Int pos, Vector3Int minPos, Vector3Int maxPos) {
+        //Only place a tile if it is on the edge
+        bool isNegX = pos.x == minPos.x;
+        bool isPosX = pos.x == maxPos.x;
+        bool isNegY = pos.y == minPos.y;
+        bool isPosY = pos.y == maxPos.y;
+        bool isNegZ = pos.z == minPos.z;
+        bool isPosZ = pos.z == maxPos.z;
+        //Fully enclosed tile, don't waste resources on a hitbox
+        if(!isNegX && !isNegY && !isNegZ && !isPosX && !isPosY && !isPosZ) {
+            return;
+        }
         GameObject tilePrefab = Resources.Load<GameObject>("Tile");
-        GameObject inst = Instantiate(tilePrefab, pos, Quaternion.identity, parent);
+        GameObject tile = Instantiate(tilePrefab, pos, Quaternion.identity, parent);
+        TileInput tileInput = tile.GetComponent<TileInput>();
+        tileInput.negativeX = isNegX;
+        tileInput.positiveX = isPosX;
+        tileInput.negativeY = isNegY;
+        tileInput.positiveY = isPosY;
+        tileInput.negativeZ = isNegZ;
+        tileInput.positiveZ = isPosZ;
+        tileInput.UpdateFaces();
     }
 }
